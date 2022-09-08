aa841780e4e8a1fe0cf70795ad4886c4632cdb78
Mathias02 <matthiasm2016@gmail.com>
2022-09-08 16:04:40 +0200

    first commit

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-}
+};
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+const movie = episodeList.map(item =>{
+ return `<p>${item.name}</p>`
+}).join('');
 }
+
 
 window.onload = setup;
